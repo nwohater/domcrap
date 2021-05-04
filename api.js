48425d40ca13e370911d9ab03a759477866a5914
nwohater <bslackey@gmail.com>
2021-05-03 22:16:14 -0700

    da

--- api.js
+++ api.js
@@ -1,58 +1,55 @@
 
 var data = '';
 
-async function fetchPerson() {
-    let response = await fetch('http://localhost:3000/people');
+    async function fetchPerson() {
+        let response = await fetch('http://localhost:3000/people');
 
-    console.log(response.status); // 200
-    console.log(response.statusText); // OK
-    
-    if (response.status === 200) {
-        data = await response.json();
-        //console.log(data);
-        createSelect(data);
-    }
-}
-
-function createSelect(data) {
-    var values = data;
- 
-    var select = document.createElement("select");
-    select.name = "people";
-    select.id = "peeps";        
-    select.addEventListener(
-        'change',
-        function() { createTable(values); },
-        false
-     );
-    console.log (values);
-    //for (const val of values)
-    for (let i = 0; i < values.length; i++) {
-        var option = document.createElement("option");
-        option.value = values[i];
-        option.text = values[i].FirstName;
-        //option.text = val.charAt(0).toUpperCase() + val.slice(1);
-        select.appendChild(option);
-      
-                
-        //Logger.log(data[i].display_name);
-    }
-    
-    var label = document.createElement("label");
-    label.innerHTML = "Choose your pets: "
-    label.htmlFor = "pets";    
-    document.getElementById("container").appendChild(label).appendChild(select);
-}
-
-function createTable(jsonData) {
-    // EXTRACT VALUE FOR HTML HEADER.         
-    var col = [];
-    for (var i = 0; i < jsonData.length; i++) {
-        for (var key in jsonData[i]) {
-            if (col.indexOf(key) === -1) {
-                col.push(key);
-            }
+        console.log(response.status); // 200
+        console.log(response.statusText); // OK
+        
+        if (response.status === 200) {
+            data = await response.json();
+            //console.log(data);
+            createSelect(data);
         }
+    }
+
+    function createSelect(data) {
+        var values = data;
+    
+        var select = document.createElement("select");
+        select.name = "people";
+        select.id = "peeps";        
+        select.addEventListener(
+            'change',
+            function() { createTable(values); },
+            false
+        );
+        console.log (values);
+        //for (const val of values)
+        for (let i = 0; i < values.length; i++) {
+            var option = document.createElement("option");
+            option.value = values[i];
+            option.text = values[i].FirstName;
+            //option.text = val.charAt(0).toUpperCase() + val.slice(1);
+            select.appendChild(option);                              
+        }
+        
+        var label = document.createElement("label");
+        label.innerHTML = "Choose your pets: "
+        label.htmlFor = "pets";    
+        document.getElementById("container").appendChild(label).appendChild(select);
+    }
+
+    function createTable(jsonData) {
+        // EXTRACT VALUE FOR HTML HEADER.         
+        var col = [];
+        for (var i = 0; i < jsonData.length; i++) {
+            for (var key in jsonData[i]) {
+                if (col.indexOf(key) === -1) {
+                    col.push(key);
+                }
+            }
     }
 
     // CREATE DYNAMIC TABLE.
@@ -66,6 +63,9 @@ function createTable(jsonData) {
         th.innerHTML = col[i];
         tr.appendChild(th);
     }
+    var th = document.createElement("th");      // TABLE HEADER.
+    th.innerHTML = 'Edit';
+    tr.appendChild(th);
 
     // ADD JSON DATA TO THE TABLE AS ROWS.
     for (var i = 0; i < jsonData.length; i++) {
@@ -74,6 +74,8 @@ function createTable(jsonData) {
             var tabCell = tr.insertCell(-1);
             tabCell.innerHTML = jsonData[i][col[j]];
         }
+        var tabCell = tr.insertCell(-1);
+        tabCell.innerHTML = 'hello world';
     }
 
     // FINALLY ADD THE NEWLY CREATED TABLE WITH JSON DATA TO A CONTAINER.
